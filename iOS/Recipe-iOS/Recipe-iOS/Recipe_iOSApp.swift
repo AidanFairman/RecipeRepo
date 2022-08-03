82f1a51e1d1086c9757c164a9aabe3c0b05077eb
Aidan Fairman <aidan@Aidans-MacBook-Pro.local>
2022-08-02 21:32:31 -0400

    adding initial iOS

---
+++ iOS/Recipe-iOS/Recipe-iOS/Recipe_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_iOSApp.swift
+//  Recipe-iOS
+//
+//  Created by Aidan Fairman on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
